5779dda58551a170708b3a2fd642c0502fe8a6a5
Fil <fil@rezo.net>
2015-03-14 14:42:42 +0000

    emoji, suite de http://core.spip.org/projects/spip/repository/revisions/21939 (il restait un bug qui plantait sur PANDA FACE

--- inc/charsets.php
+++ inc/charsets.php
@@ -662,7 +662,7 @@ function caractere_utf_8($num) {
 	if($num<65536)
 		return chr(($num>>12)+224).chr((($num>>6)&63)+128).chr(($num&63)+128);
 	if($num<1114112)
-		return chr(($num>>18)+240).chr((($num>>12)&63)+128).chr(($num>>6)&63+128). chr($num&63+128);
+		return chr(($num>>18)+240).chr((($num>>12)&63)+128).chr(($num>>6)&63+128). chr(($num&63)+128);
 	return '';
 }
 
