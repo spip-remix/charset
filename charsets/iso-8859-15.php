3a69ef0f9d60f4f455de182147a374c71ddcf626
Cerdic <cedric@yterium.com>
2015-12-13 11:16:30 +0000

    Indentation et regles de codage selon http://www.spip.net/fr_article3497.html#regles_codage

--- charsets/iso-8859-15.php
+++ charsets/iso-8859-15.php
@@ -14,23 +14,25 @@
  * Déclaration des correspondances entre charsets iso-8859-15 et unicode
  *
  * @author Gaetan Ryckeboer <gryckeboer@virtual-net.fr>
- * 
+ *
  * @package SPIP\Core\Charsets
-**/
+ **/
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 load_charset('iso-8859-1');
 
 $trans = $GLOBALS['CHARSET']['iso-8859-1'];
-$trans[164]=8364;
-$trans[166]=352;
-$trans[168]=353;
-$trans[180]=381;
-$trans[184]=382;
-$trans[188]=338;
-$trans[189]=339;
-$trans[190]=376;
+$trans[164] = 8364;
+$trans[166] = 352;
+$trans[168] = 353;
+$trans[180] = 381;
+$trans[184] = 382;
+$trans[188] = 338;
+$trans[189] = 339;
+$trans[190] = 376;
 
 $GLOBALS['CHARSET']['iso-8859-15'] = $trans;
 
--- charsets/iso-8859-9.php
+++ charsets/iso-8859-9.php
@@ -14,21 +14,23 @@
  * Déclaration des correspondances entre charsets iso-8859-9 (Turc) et unicode
  *
  * @author <alexis@nds.k12.tr>
- * 
+ *
  * @package SPIP\Core\Charsets
-**/
+ **/
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 load_charset('iso-8859-1');
 
 $trans = $GLOBALS['CHARSET']['iso-8859-1'];
-$trans[240]=287; //gbreve
-$trans[208]=286; //Gbreve
-$trans[221]=304; //Idot
-$trans[253]=305; //inodot
-$trans[254]=351; //scedil
-$trans[222]=350; //Scedil
+$trans[240] = 287; //gbreve
+$trans[208] = 286; //Gbreve
+$trans[221] = 304; //Idot
+$trans[253] = 305; //inodot
+$trans[254] = 351; //scedil
+$trans[222] = 350; //Scedil
 
 $GLOBALS['CHARSET']['iso-8859-9'] = $trans;
 
